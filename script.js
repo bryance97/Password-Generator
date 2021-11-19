8ad2050c7df7db67fcf0f051e12110a54f0b4764
bryance97 <bryancifuentesescobar@gmail.com>
2021-11-18 20:14:55 -0500

    adding input arrays

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+  // array of special characters
+  var specialCharacters = ["@","%","+","\\","/","'","!","#","$","^","?",":",",",")","(","}","{","]","[","~","-","_",".",];
+
+  // array of numbers
+  var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  // array of lower case letters
+  var lowerCasedCharacters = ["abcdefghijklmnopqrstuvwxyz"];
+
+  // array of upper case letters
+  var upperCasedCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+
+function generatePassword() {
+  //ask the user how many characters
+  //ask the user if they want numbers
+  //ask the user if they want special characters
+  //ask the user if they want uppercase letters
+  //ask the user if they want lower case letters
+  // if numbers, append numbers to character array
+  // if sc, append special characters to character array
+  // if ul, append upper case letetrs to characters array
+  // if ll, append lowercase letters
+  //generate the password
+  //repeat based on number of desired characters
+  //add random character from character array to password
+  //return the password
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,7 +36,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
